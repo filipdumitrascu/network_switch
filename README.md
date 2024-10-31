3baef054e1635b4eef009a5a80ca72d48c638d48
filipdumitrascu <filip.dumitrascu@gmail.com>
2024-10-31 20:30:27 +0200

    Implemented STP with a bug

--- README.md
+++ README.md
@@ -13,10 +13,9 @@
 ## MAC table
 The content addressable memory table (all mac-port associations) for the switch
 is stored in a hashmap `mac_table` that maps the mac address to the
-corresponding port. The process of switching frames takes place as follows
+corresponding port. The process of switching frames takes place as follows.
 When a frame is received, the `src_mac` is stored with the interface on which
-the frame was received in the switch table and the following decision
-is taken depending on the destination mac address:
+the frame was received in the switch table and the following decision is taken depending on the destination mac address:
 - If `dest_mac` is the broadcast address (`FF:FF:FF:FF:FF:FF`) the frame is
 sent on all the other ports.
 - If `dest_mac` is a unicast address, the switch searches the table for the
@@ -26,20 +25,20 @@ interface. If not, it sends the frame on all the other ports.
 
 
 ## VLAN
-The switch configuration file is parsed and each association between the switch interface and a vlan id is stored in a hashmap `vlan_configs`. Upon receiving a frame, the switch parses the header and determines the ethertype. (If the
+The switch configuration file is parsed and each association between the switch interface and a vlan id is stored in a hashmap `vlan_table`. Upon receiving a frame, the switch parses the header and determines the ethertype. (If the
 ethertype is 802.1Q, the header contains the 4 bytes of the vlan and the
-`vlan_id` variable gets the vlan id of the `recv_interface`). After `recv_interface` is decided, the following cases are handled before sending the frame: 
-- If `vlan_id` is left -1 it means that `recv_interface` is an access interface.
-    - In this way, if `dest_interface` is also an access interface, it will
+`vlan_id` variable gets the vlan id of the `recv_intrf`). After `recv_intrf` is decided, the following cases are handled before sending the frame: 
+- If `vlan_id` is left -1 it means that `recv_intrf` is an access interface.
+    - In this way, if `dest_intrf` is also an access interface, it will
     stop sending the packet only if the two interfaces do not have the same
     vlan id. 
-    - Otherwise, `dest_interface` is a trunk interface, and the 4 bytes
-    containing the vlan id must be appended to the header
+    - Otherwise, `dest_intrf` is a trunk interface, and the 4 bytes
+    containing the vlan id must be appended to the header.
 
-- If `vlan_id` has changed, it means that `recv_interface` is a trunk interface
+- If `vlan_id` has changed, it means that `recv_intrf` is a trunk interface
 and the header contains the 4 vlan bytes.
-    - If the `dest_interface` is an access interface, again it will only stop sending the packet when the two interfaces do not have the same vlan id and will remove the 4 vlan bytes in the header.
-    - Otherwise, the `dest_interface` is also a trunk interface and nothing
+    - If the `dest_intrf` is an access interface, again it will only stop sending the packet when the two interfaces do not have the same vlan id and will remove the 4 vlan bytes in the header.
+    - Otherwise, the `dest_intrf` is also a trunk interface and nothing
     extra needs to be checked.
 
 ## STP
--- switch.py
+++ switch.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python3
+# 333CA Dumitrascu Filip-Teodor
 import sys
 import struct
 import wrapper
@@ -25,28 +25,81 @@ def parse_ethernet_header(data):
 
     return dest_mac, src_mac, ether_type, vlan_id
 
-def parse_config_file(switch_id, vlan_configs):
-    with open(f"./configs/switch{switch_id}.cfg") as fin:
+def parse_config_file(sw_id, vlan_table):
+    with open(f"./configs/switch{sw_id}.cfg") as fin:
         lines = fin.readlines()
 
     # first line int the file is the switch priority
-    switch_priority = int(lines[0].strip())
+    sw_priority = int(lines[0].strip())
 
     # next ones are "interface vlanid" format 
     for line in lines[1:]:
-        interface_name, vlan_id = line.strip().split()
+        intrf_name, vlan_id = line.strip().split()
 
         # trunk interfaces
         if vlan_id == 'T':
-            vlan_configs[interface_name] = vlan_id
+            vlan_table[intrf_name] = vlan_id
         else:
-            vlan_configs[interface_name] = int(vlan_id)
+            vlan_table[intrf_name] = int(vlan_id)
     
-    return switch_priority
+    return sw_priority
+
+def parse_bpdu_frame(data, recv_intrf, own_brd_id, root_brd_id, root_pth_cost,
+                     intrfs, intrfs_sts, vlan_table, root_intrf):
+    recv_name = get_interface_name(recv_intrf)
+    was_root = (own_brd_id == root_brd_id)
+
+    dest_mac, src_mac, bpdu_own_brd_id, bpdu_root_brd_id, \
+    bpdu_root_pth_cost = struct.unpack("!6s6sIII", data)
+
+    if bpdu_root_brd_id < root_brd_id:
+        root_brd_id = bpdu_root_brd_id
+        root_pth_cost = bpdu_root_pth_cost + 10
+        root_intrf = recv_intrf
+
+        if was_root:
+            for i in intrfs:
+                i_name = get_interface_name(i)
+
+                if vlan_table[i_name] == 'T' and i != recv_intrf:
+                    intrfs_sts[i_name] = "blocking"
+        
+        if intrfs_sts[recv_name] == "blocking":
+            intrfs_sts[recv_name] = "listening"
+
+        bpdu, bpdu_length = create_bpdu(own_brd_id, root_brd_id, root_pth_cost)
+        
+        for i in intrfs:
+            i_name = get_interface_name(i)
+
+            if vlan_table[i_name] == 'T' and i != recv_intrf:
+                send_to_link(i, bpdu_length, bpdu)
+    
+    elif bpdu_root_brd_id == root_brd_id:
+        if recv_intrf == root_intrf and bpdu_root_pth_cost + 10 < root_pth_cost:
+            root_pth_cost = bpdu_root_pth_cost + 10
+
+        elif recv_intrf != root_intrf:
+            if bpdu_root_pth_cost > root_pth_cost:
+                if intrfs_sts[get_interface_name(recv_intrf)] != "listening":
+                    intrfs_sts[get_interface_name(recv_intrf)] == "listening"
+
+    elif bpdu_own_brd_id == own_brd_id:
+        intrfs_sts[recv_name] = "blocking"
+
+    if own_brd_id == root_brd_id:
+        for i in intrfs:
+            intrfs_sts[get_interface_name(i)] = "listening"
+
 
 def is_unicast(mac):
     # checks if the most significant byte is even
     return int(mac.split(":")[0], 16) % 2 == 0 
+
+def is_bpdu(mac):
+    # checks if the dest mac address identifies a bpdu frame 
+    return mac == "01:80:c2:00:00:00"
+
 
 def create_vlan_tag(vlan_id):
     # 0x8100 for the Ethertype for 802.1Q
@@ -59,70 +112,115 @@ def add_vlan_tag(length, data, vlan_id):
 def remove_vlan_tag(length, data):
     return length - 4, data[0:12] + data[16:]
 
-def send_bdpu_every_sec():
+def create_bpdu(own_brd_id, root_brd_id, root_pth_cost):
+    bpdu_data = struct.pack("01:80:c2:00:00:00", get_switch_mac(), own_brd_id,
+                            root_brd_id, root_pth_cost)
+    
+    return bpdu_data, len(bpdu_data)
+
+
+def send_bdpu_every_sec(own_brd_id, root_brd_id, root_pth_cost, intrfs,
+                        vlan_table):
     while True:
+        if own_brd_id == root_brd_id:
+            bpdu, length = create_bpdu(own_brd_id, root_brd_id, root_pth_cost)
+
+            for i in intrfs:
+                if vlan_table[get_interface_name(i)] == 'T':
+                    send_to_link(i, length, bpdu)
+
         time.sleep(1)
 
-def forward_frame(dest_interface, length, data, vlan_id, vlan_configs,
-                  recv_interface):
-    dest_name = get_interface_name(dest_interface)
-    recv_name = get_interface_name(recv_interface)
+def forward_frame(dest_intrf, length, data, vlan_id, vlan_table, recv_intrf,
+                  intrfs_sts):
+    recv_name = get_interface_name(recv_intrf)
+    dest_name = get_interface_name(dest_intrf)
 
-    if vlan_id == -1: # recv port is access
-        if vlan_configs[dest_name] != 'T': # send port is access
-            if vlan_configs[recv_name] != vlan_configs[dest_name]:
-                return
-        else: # send port is trunk
-            length, data = add_vlan_tag(length, data, vlan_configs[recv_name])
+    # recv_intrf and dest_intrf are both access
+    if vlan_id == -1 and vlan_table[dest_name] != 'T':
+        if vlan_table[recv_name] != vlan_table[dest_name]:
+            return
+    
+    # recv_intrf is access and dest_intrf is trunk
+    if vlan_id == -1 and vlan_table[dest_name] == 'T':
+        # if intrfs_sts[dest_name] == "blocking":
+        #     return
+        length, data = add_vlan_tag(length, data, vlan_table[recv_name])
 
-    else: # recv port is trunk
-        if vlan_configs[dest_name] != 'T': # send port is access
-            if vlan_configs[dest_name] != vlan_id:
-                return
-            length, data = remove_vlan_tag(length, data)
+    # recv_intrf is trunk and dest_intrf is access
+    if vlan_id != -1 and vlan_table[dest_name] != 'T':
+        if vlan_id != vlan_table[dest_name]:
+            return
+        length, data = remove_vlan_tag(length, data)
 
-    send_to_link(dest_interface, length, data)
+    # recv_intrf and dest_intrf are both trunk
+    # if vlan_id != -1 and vlan_table[dest_name] == 'T':
+    #     if intrfs_sts[dest_name] == "blocking":
+    #         return
+
+    send_to_link(dest_intrf, length, data)
 
 def main():
     mac_table = {}
-    vlan_configs = {}
+    vlan_table = {}
+    intrfs_sts = {}
     
     # init returns the max interface number. Our interfaces
     # are 0, 1, 2, ..., init_ret value + 1
-    switch_id = sys.argv[1]
-    interfaces = range(0, wrapper.init(sys.argv[2:]))
+    sw_id = sys.argv[1]
+    intrfs = range(0, wrapper.init(sys.argv[2:]))
 
-    switch_priority = parse_config_file(switch_id, vlan_configs)
+    sw_priority = parse_config_file(sw_id, vlan_table)
+
+    # all trunk ports(between switches) are set on blocking and
+    # the access ones(between a switch and a host) are set on listening
+    for i in intrfs:
+        i_name = get_interface_name(i)
+
+        intrfs_sts[i_name] = "listening" if vlan_table[i_name] != 'T' \
+            else "blocking"
+
+    own_brd_id = sw_priority
+    root_brd_id = own_brd_id
+    root_pth_cost = 0
+    root_intrf = -1
 
     # Create and start a new thread that deals with sending BDPU
-    t = threading.Thread(target=send_bdpu_every_sec)
+    t = threading.Thread(target=send_bdpu_every_sec, 
+                         args=(own_brd_id, root_brd_id, root_pth_cost, intrfs,
+                               vlan_table))
     t.start()
 
     while True:
-        recv_interface, data, length = recv_from_any_link()
+        recv_intrf, data, length = recv_from_any_link()
         dest_mac, src_mac, ethertype, vlan_id = parse_ethernet_header(data)
 
         dest_mac = ':'.join(f'{b:02x}' for b in dest_mac)
         src_mac = ':'.join(f'{b:02x}' for b in src_mac)
 
-        mac_table[src_mac] = recv_interface
+        mac_table[src_mac] = recv_intrf
 
         if is_unicast(dest_mac):
             if dest_mac in mac_table:
                 forward_frame(mac_table[dest_mac], length, data, vlan_id,
-                              vlan_configs, recv_interface)
+                              vlan_table, recv_intrf, intrfs_sts)
 
             else:
-                for i in interfaces:
-                    if i != recv_interface:
-                        forward_frame(i, length, data, vlan_id, vlan_configs, 
-                                      recv_interface)
+                for i in intrfs:
+                    if i != recv_intrf:
+                        forward_frame(i, length, data, vlan_id, vlan_table, 
+                                      recv_intrf, intrfs_sts)
 
-        else:
-            for i in interfaces:
-                if i != recv_interface:
-                    forward_frame(i, length, data, vlan_id, vlan_configs,
-                                  recv_interface)
+        elif is_bpdu(dest_mac):
+            parse_bpdu_frame(data, recv_intrf, own_brd_id, root_brd_id,
+                             root_pth_cost, intrfs, intrfs_sts, vlan_table,
+                             root_intrf)
+
+        else: # is broadcast
+            for i in intrfs:
+                if i != recv_intrf:
+                    forward_frame(i, length, data, vlan_id, vlan_table,
+                                  recv_intrf, intrfs_sts)
 
 if __name__ == "__main__":
     main()
